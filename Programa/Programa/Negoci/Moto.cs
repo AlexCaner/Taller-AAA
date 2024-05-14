3902f49b49647c5a47a23824b0b7830d578acf4f
a2308759@institutmontilivi.cat <a2308759@institutmontilivi.cat>
2024-05-14 09:57:16 +0200

    Projecte base amb classes creades.

---
+++ Programa/Programa/Negoci/Moto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa.Classes
+{
+    internal class Moto:Vehicle
+    {
+        //Atributs i Propietats
+        public string tipusMoto {  get; set; }
+        public int potencia {  get; set; }
+        public string tipusMotor {  get; set; }
+    }
+}
