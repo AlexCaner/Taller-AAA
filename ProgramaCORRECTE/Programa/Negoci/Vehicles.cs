9fae54d7a3c8a72481207d074457ec42dea61267
Acastillo <a2308932@institutmontilivi.cat>
2024-05-21 20:44:41 +0200

    Copia Programa Proces (2) Si no funciona fer pull.

---
+++ ProgramaCORRECTE/Programa/Negoci/Vehicles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa.Classes
+{
+    internal class Vehicles
+    {
+        //Atributs i Propietats
+        List<Vehicle> vehicles {  get; set; }
+
+        //Constructors
+        public Vehicles()
+        {
+            vehicles = new List<Vehicle>();
+        }
+    }
+}
